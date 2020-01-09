0521c0d8017a86d6900431a62b1a9f54da6ee294
Dante Catalfamo <dante.catalfamo@shopify.com>
2020-01-09 15:07:25 -0500

    Use tmux to prevent the terminal from getting messed up by emacs

--- agenda-html.sh
+++ agenda-html.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 cd $(dirname "$0")
-TERM=xterm emacs -Q -nw -l ./agenda-html.el --eval '(kill-emacs)'
+tmux new-session -d -s agendahtml
+tmux send-keys -t agendahtml.0 "TERM=xterm emacs -Q -nw -l ./agenda-html.el --eval '(kill-emacs)'" C-m
+tmux send-keys -t agendahtml.0 "exit" C-m
