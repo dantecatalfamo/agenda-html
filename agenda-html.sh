54413ea97fa5d0a048139623420acac5505ceacd
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-01-09 19:40:40 -0500

    Wait for emacs to close before sending exit to tmux

--- agenda-html.sh
+++ agenda-html.sh
@@ -3,4 +3,5 @@
 cd $(dirname "$0")
 tmux new-session -d -s agendahtml
 tmux send-keys -t agendahtml.0 "TERM=xterm emacs -Q -nw -l ./agenda-html.el --eval '(kill-emacs)'" C-m
+sleep 4
 tmux send-keys -t agendahtml.0 "exit" C-m
