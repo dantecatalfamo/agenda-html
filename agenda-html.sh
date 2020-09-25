186e421353018fb30ce789ff832ae30df0df7211
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-09-24 22:12:11 -0400

    Don't guess sleep length, use named pipe instead

--- agenda-html.sh
+++ agenda-html.sh
@@ -2,7 +2,10 @@
 
 cd $(dirname "$0")
 git clone https://github.com/hniksic/emacs-htmlize
+PIPENAME=$(mktemp -ut agenda.XXXXXXXXXX)
+mkfifo $PIPENAME
 tmux new-session -d -s agendahtml
-tmux send-keys -t agendahtml.0 "TERM=xterm emacs -Q -nw -l ./agenda-html.el --eval '(kill-emacs)'" C-m
-sleep 4
+tmux send-keys -t agendahtml.0 "TERM=xterm emacs -Q -nw -l ./agenda-html.el --eval '(kill-emacs)' && echo done > $PIPENAME" C-m
+cat $PIPENAME > /dev/null
+rm $PIPENAME
 tmux send-keys -t agendahtml.0 "exit" C-m
