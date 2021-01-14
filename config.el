0a94abdd546529e77e3c8faa285658746120b616
Dante Catalfamo <dante.catalfamo@gmail.com>
2021-01-14 00:35:35 -0500

    Disable text output by default

--- config.el
+++ config.el
@@ -1,4 +1,4 @@
 (setq org-agenda-files '("~/Org/Personal.org" "~/Org/Work.org"))
 (setq agenda-html-file "~/agenda.html")
-(setq agenda-text-file "~/agenda.txt")
+(setq agenda-text-file nil)
 (setq agenda-arrows t)
