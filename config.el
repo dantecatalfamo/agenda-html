e8c50c192a2bd74aa2b72b782d51040998f0ccf5
Dante Catalfamo <dante.catalfamo@gmail.com>
2021-01-13 19:00:24 -0500

    Change name of org file in example config

--- config.el
+++ config.el
@@ -1,3 +1,3 @@
-(setq org-agenda-files '("~/Org/Personal.org" "~/Org/Shopify.org"))
+(setq org-agenda-files '("~/Org/Personal.org" "~/Org/Work.org"))
 (setq agenda-html-file "~/agenda.html")
 (setq agenda-text-file "~/agenda.txt")
