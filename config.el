5868d7c75de6475dd7763b3356b5d9d47acd1ef6
Dante Catalfamo <dante.catalfamo@shopify.com>
2021-09-14 17:43:33 -0400

    Lisp symbol should be list

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 ; (setq org-agenda-start-on-weekday nil) ;; Current date at top of agenda
 ; (setq org-agenda-span 'fortnight) ;; How much to show. Can be day, week, month, year, or any number of days.
 ; (setq org-icalendar-exclude-tags '("noexport")) ;; Exclude headlines with these tags from iCal export
-(setq org-icalendar-use-scheduled 'event-if-todo)
+(setq org-icalendar-use-scheduled '(event-if-todo))
 (setq org-agenda-files '("~/Org/Personal.org" "~/Org/Work.org"))
 (setq agenda-html-file "~/agenda.html")
 (setq agenda-text-file nil)
